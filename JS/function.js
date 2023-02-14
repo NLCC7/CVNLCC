48db2c24ee54e1f7c684b4aeaf0b4ef9c8c6ff43
Nadia Carrizo <cnlcarrizo@sanluis.edu.ar>
2023-02-14 11:22:55 -0300

    cvnlcc

---
+++ JS/function.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById(button).onclick = function () {
+    console.log ("Capturamos el click");
+    document.getElementById('demo').innerHTML = "probando script" ;
+}
---
+++ JS/sintaxis.js
@@ -0,0 +1,11 @@
+
+var miVariable/**cualquier variable x, i , etc -> 
+estas cambian a lo largo de la ejecucion de un programa 
+let nombre // 'var' dejo de usarse, sirve para navegadores viejos 
+as variables deben ser identificadas con un nombre unico
+ejemplo''const IVA '' Es una variable que no cambia constante debe 
+ser inicializada por ende su valor no sera afectado desde que empiece hasta que
+ termine el programa
+*/ 
+let 
+cons
